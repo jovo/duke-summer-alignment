82d3274f0bd8397b783502eb3ccab17c7e7fa4d0
Roger <rogerzou94@gmail.com>
2014-05-28 16:53:32 -0400

    add diff to tests

--- src/tests/rough_alignment_user_test.m
+++ src/tests/rough_alignment_user_test.m
@@ -5,7 +5,12 @@
 % name it 'im'.
 
 imnew = roughalign(im);
+% show diff for each pair.
+for i=1:size(imnew, 3)-1
+    figure; imshowpair(imnew(:,:,i), imnew(:,:,i+1), 'diff');
+end
 
 % this should display error metrics for the new and original cubes and some
 % error statistics. 
-% imnew is the new stack of aligned images. 
+% imnew is the new stack of aligned images.
+% the diff between each pair should be outputted.
