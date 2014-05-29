09b034278a9f897e3995b7c51e787ed0f20eb65f
Roger <rogerzou94@gmail.com>
2014-05-29 16:31:27 -0400

    update user test for rough alignment

--- src/tests/rough_align_user_test.m
+++ src/tests/rough_align_user_test.m
@@ -3,9 +3,10 @@
 
 % 1) Name the image cube you wish to run with rough_align.m to 'im'
 % 2) run roughalign(im);
-imnew = roughalign(im);
+[merged, transforms] = roughalign(im, 'align');
 % 3) wait. depending on cube size, this may take a while. 
 % 4) once done running, error metrics showing the difference in MSE is
 % displayed for each adjacent pair. An error report summary for original
 % and aligned cubes should also be displayed.
-% 5) the variable for aligned cube is 'imnew'
+% 5) the variable for aligned cube is 'merged'. The variable 'transforms'
+% has the table of transforms in a matlab map data structure.
