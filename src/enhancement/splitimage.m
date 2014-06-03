5152644d0bdccf2a2e1cf4c645e77cf64ada0c54
Roger <rogerzou94@gmail.com>
2014-06-03 13:29:56 -0400

    restructure ransac files

---
+++ src/enhancement/splitimage.m
@@ -0,0 +1,11 @@
+function [ separated ] = splitimage( image, m, b )
+%SPLITIMAGE splits the image
+%   Detailed explanation goes here
+
+
+matrix1
+matrix2
+separated = cat(3, matrix1, matrix2);
+
+end
+
