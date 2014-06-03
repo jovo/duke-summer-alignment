1278ab98b26e42334339f786cf6640f311b68141
Roger <rogerzou94@gmail.com>
2014-06-03 11:37:28 -0400

    add API support to demo

--- src/tests/rough_align_user_test.m
+++ src/tests/rough_align_user_test.m
@@ -1,6 +1,12 @@
 %% USER TEST SCRIPT FOR ROUGH_ALIGN.M
 % Read the instructions on this script. Then run this script.
 
+% 0) To download an example 1024 by 1024 by 100 cube from the API, run
+% read_api. If you don't wish to use the API, comment out the below two
+% lines. If there is a specific cube you wish to use, modify the read_api
+% code accordingly.
+cutout = read_api;
+im = cutout.data;
 % 1) Name the image cube you wish to run with rough_align.m to 'im'
 % 2) the scale is set to 0.5 right now. This means that each image is
 % scaled by 0.5 before computing the transforms. This improves the running
