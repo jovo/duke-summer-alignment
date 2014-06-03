fa7440941a4cd4eb07a8c04ec1f670203d5d0afa
julesknee <julia.ni.2010@gmail.com>
2014-06-03 15:20:03 -0400

    Update splitimage.m

--- src/enhancement/splitimage.m
+++ src/enhancement/splitimage.m
@@ -1,7 +1,13 @@
-function [ separated ] = splitimage( image, m, b )
+function [separated] = splitimage(image, m, b)
 %SPLITIMAGE splits the image
-%   Detailed explanation goes here
+%   Splits image into two pieces, alternatively setting the coefficients above and below the line of best fit to zero
 
+%[X,Y]=[row,column]
+[Y,X] = ind2sub(size(image),1:size(image,1)*size(image,2)); 
+fpiece = [X; Y; Y>m.*X+b]';
+spiece = [X; Y; Y<m.*X+b]'; 
+
+%to be continued 
 
 matrix1
 matrix2
