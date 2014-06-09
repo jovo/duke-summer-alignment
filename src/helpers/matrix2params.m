5daee1c06be7ab4f300f9bf67d39ddd208b2938b
julesknee <julia.ni.2010@gmail.com>
2014-06-09 11:43:43 -0400

    Update matrix2params.m

--- src/helpers/matrix2params.m
+++ src/helpers/matrix2params.m
@@ -1,7 +1,8 @@
 function [ params ] = matrix2params( matrix )
 %MATRIX2PARAMS converts transformation matrix to vector of parameters
-%   [params] = matrix2params(matrix) -- takes in transformation matrix dtm'd 
-%   by Rotation*Translation and converts it into vector of parameters, 
+%   [params] = matrix2params(matrix) -- takes in transformation matrix 
+%   dtm'd by R*T, where TranslateX = T(3,1) and TranslateY = T(3,2) and
+%   converts it into vector of parameters,
 %   params = [TranslateY, TranslateX, THETA, SCALE, 0]
 
 TranslateX = matrix(3, 1);
