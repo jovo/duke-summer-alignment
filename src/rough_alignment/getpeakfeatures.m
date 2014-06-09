e42a0fc93e66ffdfd3d1e24d11aaa92fc6efc64a
julesknee <julia.ni.2010@gmail.com>
2014-06-09 16:18:05 -0400

    Update getpeakfeatures.m

--- src/rough_alignment/getpeakfeatures.m
+++ src/rough_alignment/getpeakfeatures.m
@@ -8,5 +8,17 @@ features(1) = Gmag(ypeak,xpeak);
 [Lmag,Ldir] = imgradient(Gmag);
 features(2) = Lmag(ypeak,xpeak);
 
+%histogram statistics  
+meanGL = mean(double(c(:)));
+varGL = var(double(c(:)));
+
+[pixelCounts GLs] = imhist(c); %grayLevels = GLs
+skew = sum((GLs-meanGL).^ 3 .*pixelCounts)/((sum(pixelCounts)-1)*varGL^1.5); 
+kurtosis = kurtosis(double(c(:)));
+
+features(3) = meanGL;
+features(4) = varGL;
+features(5) = skew; %negative skew = skewed left 
+features(6) = kurtosis; 
 
 end
