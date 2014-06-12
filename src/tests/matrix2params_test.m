af5d1589d4b77daa795c403a3102545a58e83e07
julesknee <julia.ni.2010@gmail.com>
2014-06-12 14:56:52 -0400

    Update matrix2params_test.m

--- src/tests/matrix2params_test.m
+++ src/tests/matrix2params_test.m
@@ -1,33 +1,39 @@
 classdef matrix2params_test < matlab.unittest.TestCase
-    %MATRIX2PARAMS_TEST compares transformations 
+    %MATRIX2PARAMS_TEST compares transformations in matrix and vector forms
     %   matrix2params_test checks to see if parameters vector contains same
     %   inputs as matrix of transformations 
-    
-    properties
-        OriginalPath 
-    end
-    
-    methods (TestMethodSetup)
-        function addSolverToPath(testCase)
-        testCase.OriginalPath = path;
-        addpath(fullfile(pwd,' '));
-        end
-    end
-    
+   
     methods (Test) 
         function testTransformations(testCase)
+            theta = rand(1)*360;
+            c1 = [cosd(theta) -sind(theta) 0];
+            c2 = [sind(theta) cosd(theta) 0];
+            matrix = [c1;c2;[randn(1,2),1]];
+            params = matrix2params(matrix);
+            
+            %transformations 
             actTx = params(2);
             expTx = matrix(3,1);
             actTy = params(1);
             expTy = matrix(3,2);
-            actTHETA = params(3);
-            expTHETA = acos(matrix(1,1)) * 180/pi;
+            
+            if params(3) < 0;
+                actTHETA = round((params(3)+360)*1e10)*(1e-10);
+                expTHETA = round((360-acosd(matrix(1,1)))*1e10)*(1e-10);
+            else
+                actTHETA = round((params(3))*1e10)*(1e-10);
+                expTHETA = round((acosd(matrix(1,1)))*(1e10))*(1e-10);
+            end
+            
             actSCALE = params(4);
             expSCALE = sqrt(matrix(1,1)^2 + matrix(2,1)^2);
+            
+            % tests for equality 
             testCase.verifyEqual(actTx, expTx);
             testCase.verifyEqual(actTy, expTy);
             testCase.verifyEqual(actTHETA, expTHETA);
             testCase.verifyEqual(actSCALE, expSCALE);
         end
     end
+    
 end
