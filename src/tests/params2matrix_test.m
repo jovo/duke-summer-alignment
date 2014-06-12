600339e298684f121129ff1c070703a0bbfe9884
julesknee <julia.ni.2010@gmail.com>
2014-06-12 16:56:11 -0400

    updated variables
    check theta again, scale 1.0000~=1

--- src/tests/params2matrix_test.m
+++ src/tests/params2matrix_test.m
@@ -3,18 +3,37 @@ classdef params2matrix_test < matlab.unittest.TestCase
     %   params2matrix_test checks to see if matrix of transformations contains same
     %   inputs as parameters vector
     
-    properties
-    end
-    
     methods (Test)
         function testTransformations(testCase)
+            theta = rand(1)*360;
+            params = [randn(1,2) theta 1 0];
+            matrix = params2matrix(params);
+            
+            % transformations 
             actTx = matrix(3,1);
             expTx = params(2);
             actTy = matrix(3,2);
             expTy = params(1);
-            actTHETA = ;
-            expTHETA = params(3);
-            actSCALE = ;
+            
+            if params(3) < 0; 
+                actTHETA = round((360-acosd(matrix(1,1)))*1e10)*(1e-10);
+                expTHETA = round((params(3)+360)*1e10)*(1e-10);
+            elseif params(3) > 180;
+                actTHETA = round((360-acosd(matrix(1,1)))*1e10)*(1e-10);
+                expTHETA = round((params(3))*1e10)*(1e-10);
+                else
+                actTHETA = round((acosd(matrix(1,1)))*1e10)*(1e-10);
+                expTHETA = round((params(3))*1e10)*(1e-10);
+            end
+                                                     
+            actSCALE = round((sqrt(matrix(1,1)^2 + matrix(2,1)^2))*1e10)*(1e-10);
             expSCALE = params(4); 
+            
+            % tests for equality 
+            testCase.verifyEqual(actTx, expTx);
+            testCase.verifyEqual(actTy, expTy);
+            testCase.verifyEqual(actTHETA, expTHETA);
+            testCase.verifyEqual(actSCALE, expSCALE);
         end
     end
+end
