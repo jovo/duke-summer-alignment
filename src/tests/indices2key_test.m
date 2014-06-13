38cc35d104124dda43ffe79254430fa7eb70850a
julesknee <julia.ni.2010@gmail.com>
2014-06-13 11:26:02 -0400

    updated vars

--- src/tests/indices2key_test.m
+++ src/tests/indices2key_test.m
@@ -4,11 +4,14 @@ classdef indices2key_test < matlab.unittest.TestCase
     
     methods (Test)
         function testTransformations(testCase)
+            num1 = randi([1,1024],1,1); 
+            num2 = randi([1,1024],1,1); 
             key = indices2key(num1,num2);
+            k = str2num(key); 
             
             % tests for equality 
-            testCase.verifyEqual(num1,key(1));
-            testCase.verifyEqual(num2, );
+            testCase.verifyEqual(num1,k(1));
+            testCase.verifyEqual(num2,k(2));
         end
     end
 end
