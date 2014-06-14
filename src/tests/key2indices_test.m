35498d917a8552e8167ab3545f95ee07d298dddb
julesknee <julia.ni.2010@gmail.com>
2014-06-14 19:00:04 -0400

    removed printing

--- src/tests/key2indices_test.m
+++ src/tests/key2indices_test.m
@@ -4,9 +4,9 @@ classdef key2indices_test < matlab.unittest.TestCase
     
     methods (Test)
         function testTransformations(testCase)
-            randkey = randi([1,1024],1,2)
-            key = num2str(randkey)
-            [num1,num2] = key2indices(key)
+            randkey = randi([1,1024],1,2);
+            key = num2str(randkey);
+            [num1,num2] = key2indices(key);
             
             % tests for equality 
             testCase.verifyEqual(randkey(1),num1);
