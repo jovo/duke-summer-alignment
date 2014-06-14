c54708e80a1da7eb131a5e6875e30de7e0d87731
julesknee <julia.ni.2010@gmail.com>
2014-06-14 18:50:50 -0400

    Update key2indices.m

--- src/helpers/key2indices.m
+++ src/helpers/key2indices.m
@@ -1,7 +1,7 @@
 function [ num1, num2 ] = key2indices( key )
 %KEY2INDICES Reverse of indices2key
 %   [num1,num2] = key2indices(key) takes in key 'x y', a string of 
-%   consecutive integers, and converts it into numbers
+%   consecutive integers, and converts it into indices 
 
 splitted = strsplit(key);
 num1 = str2double(splitted{1});
