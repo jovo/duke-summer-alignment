1f71a8c2f00f4c0acd43791cdc66e62bf3a91d99
julesknee <julia.ni.2010@gmail.com>
2014-06-14 18:43:49 -0400

    updated description

--- src/helpers/params2matrix.m
+++ src/helpers/params2matrix.m
@@ -1,6 +1,6 @@
 function [ matrix ] = params2matrix( params )
 %PARAMS2MATRIX Convert transformation parameters to matrix form
-%   Detailed explanation goes here
+%   Takes vector of parameters and converts into 3-by-3 matrix
 
 TranslateY = params(1);
 TranslateX = params(2);
