0ba07322f61f37204b12460ca62311dece3bfcff
julesknee <julia.ni.2010@gmail.com>
2014-06-14 18:37:54 -0400

    Edited indices

--- src/tests/key2indices_test.m
+++ src/tests/key2indices_test.m
@@ -4,12 +4,13 @@ classdef key2indices_test < matlab.unittest.TestCase
     
     methods (Test)
         function testTransformations(testCase)
-            [num1,num2] = key2indices(key);
-            split = strsplit(key);
+            randkey = randi([1,1024],1,2)
+            key = num2str(randkey)
+            [num1,num2] = key2indices(key)
             
             % tests for equality 
-            testCase.verifyEqual(split{1},num1);
-            testCase.verifyEqual(split{2},num2);
+            testCase.verifyEqual(randkey(1),num1);
+            testCase.verifyEqual(randkey(2),num2);
         end
     end
 end
