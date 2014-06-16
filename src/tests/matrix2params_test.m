ba271734bbb1317da00d63e11192ef10e93ca151
julesknee <julia.ni.2010@gmail.com>
2014-06-15 21:08:14 -0400

    updated to reflect new corresp func

--- src/tests/matrix2params_test.m
+++ src/tests/matrix2params_test.m
@@ -5,34 +5,38 @@ classdef matrix2params_test < matlab.unittest.TestCase
    
     methods (Test) 
         function testTransformations(testCase)
-            % specify range [a,b] of randomly generated translations
-            a = -10;
-            b = 10;
-            
             theta = (2*rand(1,1)-1)*360;
-            c1 = [cosd(theta) -sind(theta) 0];
-            c2 = [sind(theta) cosd(theta) 0];
-            c3 = [(b-a).*rand(1,2)+a,1];
-            matrix = [c1;c2;c3]; 
+            trans = [...
+                       1,  0,  0; ...
+                       0,  1,  0; ...
+                       randi([-10,10],[1,2]) , 1 ...
+                    ]; % assumes range of translations = [-10,10]
+            rot = [...
+                     cosd(theta),  sind(theta), 0; ...
+                     -sind(theta), cosd(theta), 0; ...
+                     0,            0,           1  ...
+                  ];
+            matrix = trans*rot;  
             params = matrix2params(matrix);
             
             % transformations 
             actTx = params(2);
-            expTx = matrix(3,1);
+            expTx = trans(3,1);
             actTy = params(1);
-            expTy = matrix(3,2);
+            expTy = trans(3,2);
             
+            % rotation 
             actTHETA = round(mod(params(3),360)*1e10)*(1e-10);
             if params(3) < 0;
                 if params(3) < -180;
-                    expTHETA = round((acosd(matrix(1,1)))*(1e10))*(1e-10);
+                    expTHETA = round((acosd(rot(1,1)))*(1e10))*(1e-10);
                 else    
-                expTHETA = round((360-acosd(matrix(1,1)))*1e10)*(1e-10);
+                expTHETA = round((360-acosd(rot(1,1)))*1e10)*(1e-10);
                 end
             elseif params(3) > 180;
-                expTHETA = round((360-acosd(matrix(1,1)))*(1e10))*(1e-10);
+                expTHETA = round((360-acosd(rot(1,1)))*(1e10))*(1e-10);
                 else
-                expTHETA = round((acosd(matrix(1,1)))*(1e10))*(1e-10);
+                expTHETA = round((acosd(rot(1,1)))*(1e10))*(1e-10);
             end
             
             % tests for equality 
