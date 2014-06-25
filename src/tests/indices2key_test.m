1b86f1a5d58967cf3cba68109aa9ee97cbef0a21
julesknee <julia.ni.2010@gmail.com>
2014-06-25 10:43:55 -0400

    updated to reflect func changes

--- src/tests/indices2key_test.m
+++ src/tests/indices2key_test.m
@@ -7,11 +7,13 @@ classdef indices2key_test < matlab.unittest.TestCase
             num1 = randi([1,1024],1,1); 
             num2 = randi([1,1024],1,1); 
             key = indices2key(num1,num2);
-            k = str2num(key); 
+            split = strsplit(key,'_');
+            k1 = str2double(split(1));
+            k2 = str2double(split(2)); 
             
             % tests for equality 
-            testCase.verifyEqual(num1,k(1));
-            testCase.verifyEqual(num2,k(2));
+            testCase.verifyEqual(num1,k1);
+            testCase.verifyEqual(num2,k2);
         end
     end
 end
