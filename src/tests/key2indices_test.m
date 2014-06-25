59559243c0c475f4349538a701b461a883df4271
julesknee <julia.ni.2010@gmail.com>
2014-06-25 11:00:16 -0400

    updated to reflect func changes

--- src/tests/key2indices_test.m
+++ src/tests/key2indices_test.m
@@ -4,13 +4,15 @@ classdef key2indices_test < matlab.unittest.TestCase
     
     methods (Test)
         function testIndices(testCase)
-            randkey = randi([1,1024],1,2);
+            rand1 = randi(1024,1);
+            rand2 = randi(1024,1);
+            randkey = [int2str(rand1),'_',int2str(rand2)];
             key = num2str(randkey);
             [num1,num2] = key2indices(key);
             
             % tests for equality 
-            testCase.verifyEqual(randkey(1),num1);
-            testCase.verifyEqual(randkey(2),num2);
+            testCase.verifyEqual(rand1,num1);
+            testCase.verifyEqual(rand2,num2);
         end
     end
 end
