387cbbe131d8c9e58a5fbb3b84d178a2561b491c
Roger <rogerzou94@gmail.com>
2014-06-25 19:45:44 -0400

    debug

--- src/init/constructimgcubealignment.m
+++ src/init/constructimgcubealignment.m
@@ -15,15 +15,19 @@ oo.setImageToken(Transforms.imgtoken);
 % retrieve sizes from API
 image_size = oo.imageInfo.DATASET.IMAGE_SIZE(Transforms.resolution);
 slicerange = oo.imageInfo.DATASET.SLICERANGE(2);
-
+% sub-cube size
 xsubsize = Transforms.xsubsize;
 ysubsize = Transforms.ysubsize;
-xcubeimgsize = min(image_size(1)-xoffset, ceil(xtotalsize/xsubsize)*xsubsize);
-ycubeimgsize = min(image_size(2)-yoffset, ceil(ytotalsize/ysubsize)*ysubsize);
-zcubeimgsize = min(slicerange-zoffset, ztotalsize);
-xoffsetmark = floor(xoffset/xsubsize)*xsubsize;
-yoffsetmark = floor(yoffset/ysubsize)*ysubsize;
+% round offset to nearest subcube
+xoffsetdiff = xoffset-Transforms.xoffset;
+yoffsetdiff = yoffset-Transforms.yoffset;
+xoffsetmark = Transforms.xoffset + floor(xoffsetdiff/xsubsize)*xsubsize;
+yoffsetmark = Transforms.yoffset + floor(yoffsetdiff/ysubsize)*ysubsize;
 zoffsetmark = zoffset;
+% compute modified total size
+xcubeimgsize = min(image_size(1)-xoffsetmark, xtotalsize+xoffset-xoffsetmark);
+ycubeimgsize = min(image_size(2)-yoffsetmark, ytotalsize+yoffset-yoffsetmark);
+zcubeimgsize = min(slicerange-zoffset, ztotalsize);
 
 % retrieve data from API
 cutout = read_api(  oo, ...
@@ -41,21 +45,21 @@ data = cutout.data;
 xItCount = ceil(xcubeimgsize/xsubsize);
 yItCount = ceil(ycubeimgsize/ysubsize);
 [xstartindex, ystartindex] = meshgrid(1:xItCount, 1:yItCount);
-xstartindex = (xstartindex(:)*xsubsize - xsubsize) + xoffsetmark;
-ystartindex = (ystartindex(:)*ysubsize - ysubsize) + yoffsetmark;
+xstartindex = (xstartindex(:)*xsubsize - xsubsize);
+ystartindex = (ystartindex(:)*ysubsize - ysubsize);
 
 % iterate over each sub-cube defined when computing transformations
 itLength = size(xstartindex, 1);
 zsliceLength = zcubeimgsize;
 for i=1:itLength
-    
+
     % set offsets and sizes
-    if xstartindex(i) == xsubsize * (xItCount-1) + xoffsetmark
+    if xstartindex(i) == xsubsize * (xItCount-1)
         xs = xcubeimgsize - xstartindex(i);
     else
         xs = xsubsize;
     end
-    if ystartindex(i) == ysubsize * (yItCount-1) + yoffsetmark
+    if ystartindex(i) == ysubsize * (yItCount-1)
         ys = ycubeimgsize - ystartindex(i);
     else
         ys = ysubsize;
@@ -72,15 +76,17 @@ for i=1:itLength
         globalkey = globalindices2key(struct( ...
                             'imgtoken', Transforms.imgtoken, ...
                             'resolution', Transforms.resolution, ...
-                            'xoffset', xoff, ...
-                            'yoffset', yoff, ...
+                            'xoffset', xoffsetmark + xoff, ...
+                            'yoffset', yoffsetmark + yoff, ...
                             'xsubsize', xs, ...
                             'ysubsize', ys, ...
-                            'zslice1', j+zoffset, ...
-                            'zslice2', j+1+zoffset ...
+                            'zslice1', zoffset + j-1, ...
+                            'zslice2', zoffset + j ...
                            ));
+                       globalkey
         try
-            val = values(Transforms.transforms, globalkey);
+            val = values(Transforms.transforms, {globalkey});
+            disp('yay');
         catch
             val = {eye(3)};
         end
@@ -109,16 +115,40 @@ for i=1:itLength
             [ypeak, xpeak] = find(c==max(c(:)));
             translatey = ypeak - size(rmaligned, 1) - yshiftmin;
             translatex = xpeak - size(rmaligned, 2) - xshiftmin;
-            ystart = yoff + translatey + 1;
-            xstart = xoff + translatex + 1;
-            yend = min(yoff + size(alignedseg, 1), size(data, 1));
-            xend = min(xoff + size(alignedseg, 2), size(data, 2));
+            ystart = max(yoff + 1 + translatey, 1);
+            xstart = max(xoff + 1 + translatex, 1);
+            yend = min(yoff + size(alignedseg, 1) + translatey, size(data, 1));
+            xend = min(xoff + size(alignedseg, 2) + translatex, size(data, 2));
 
             % get image cube to apply transformations
             replaced = data(ystart:yend, xstart:xend, :);
-            
+
             % replace image cube with aligned data only w/ non-zero entries
-            alignedseg = alignedseg(1:min(size(replaced,1), end), 1:min(size(replaced,2), end), :);
+            if ystart + translatey < 1
+                aliystart = 1 - translatey;
+            else
+                aliystart = 1;
+            end
+            if xstart + translatex < 1
+                alixstart = 1 - translatex;
+            else
+                alixstart = 1;
+            end
+            yendtemp = ystart + translatey + size(alignedseg, 1);
+            if yendtemp > size(data, 1)
+                aliyend = size(alignedseg, 1) - (yendtemp - size(data, 1)) + 1;
+            else
+                aliyend = size(alignedseg, 1);
+            end
+            xendtemp = xstart + translatex + size(alignedseg, 2);
+            if xendtemp > size(data, 2)
+                alixend = size(alignedseg, 2) - (xendtemp - size(data, 2)) + 1;
+            else
+                alixend = size(alignedseg, 2);
+            end
+
+            alignedseg = alignedseg(aliystart:aliyend, alixstart:alixend, :);
+
             notzero = alignedseg~=0;
             replaced(notzero) = alignedseg(notzero);
             data(ystart:yend, xstart:xend, :) = replaced;
