8a10006559c0620f4a23e01b8888cd204ef9b0a8
Roger <rogerzou94@gmail.com>
2014-07-01 15:58:01 -0400

    update README

--- src/run/read_api.m
+++ src/run/read_api.m
@@ -2,6 +2,7 @@ function [ cutout ] = read_api(oo, xoff, yoff, zoff, xsize, ysize, zsize, res)
 %READ_API Read the API and output the stack of images
 %   [ cutout ] = read_api(oo, xoff, yoff, zoff, xsize, ysize, zsize, res)
 %   Retrieves data from the API.
+
 % configure settings
 oo.setDefaultResolution(res);
 q = OCPQuery(eOCPQueryType.imageDense);
