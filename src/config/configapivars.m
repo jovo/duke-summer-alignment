348f9376638c58c65c21324f707bf3cad31b2a9b
Roger <rogerzou94@gmail.com>
2014-07-07 13:51:37 -0400

    urgent bug fix

--- src/config/configapivars.m
+++ src/config/configapivars.m
@@ -8,7 +8,7 @@ config = struct;
 config.imgtoken = 'kasthuri11cc';
 
  % resolution
-config.resolution = 1;
+config.resolution = 3;
 
 % total size of cube to-be aligned. Any number outside the range of the
 % data set will automatically be ended at the range of the data set.
@@ -24,11 +24,11 @@ config.zsubsize = 2;
 % the overall offset. Set to 0 for no offset.
 config.xoffset = 0;
 config.yoffset = 0;
-config.zoffset = 0;
+config.zoffset = 1;
 
 % if parallelize is true (1), then will use parallel computing using the #
 % of workers specified in workersize.
 config.parallelize = 1;
-config.workersize = 6;
+config.workersize = 3;
 
 end
