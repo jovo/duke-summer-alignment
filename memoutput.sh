ba8cc4167a921c5e9dfa53e3fce0c97f27a7a660
Roger <rogerzou94@gmail.com>
2014-07-21 11:40:12 -0400

    remove cached

--- memoutput.sh
+++
@@ -1,5 +0,0 @@
-while :
-do
-		top -b -p $(pgrep -d, MATLAB -U rtg-7) >> topoutput.txt
-		sleep 5m
-done
--- src/main/helpers/xcorr/highpass.m
+++
@@ -1,8 +0,0 @@
-function [ M_hip ] = highpass( M )
-%HIGHPASS Applies high-pass emphasis filter to image M
-
-X = cos(linspace(-0.5,0.5,size(M,1)))'*cos(linspace(-0.5,0.5, size(M,2)));
-H = (1-X).*(2-X);   % transfer function
-M_hip = M.*H;
-
-end
